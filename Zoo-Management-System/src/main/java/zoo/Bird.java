06fef5c2cefdbada0511c5ee39e9d279fa6d7ecb
Ranjith <ranjiaiinnovator@gmail.com>
2025-10-20 07:30:47 +1100

    feat: add Animal hierarchy and ZooManager classes

--- Zoo-Management-System/src/main/java/zoo/Bird.java
+++ Zoo-Management-System/src/main/java/zoo/Bird.java
@@ -1,4 +1,18 @@
 package zoo;
 
-public class Bird {
+public class Bird extends Animal {
+
+    public Bird(String name, int age) {
+        super(name, age, "Bird");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(name + " pecks at seeds and fruits.");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " chirps melodiously.");
+    }
 }
--- Zoo-Management-System/src/main/java/zoo/Reptile.java
+++ Zoo-Management-System/src/main/java/zoo/Reptile.java
@@ -1,4 +1,18 @@
 package zoo;
 
-public class Reptile {
+public class Reptile extends Animal {
+
+    public Reptile(String name, int age) {
+        super(name, age, "Reptile");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(name + " snaps up insects or small animals.");
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " hisses softly.");
+    }
 }
