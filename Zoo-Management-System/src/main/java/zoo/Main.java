dff662a022aa81c953a525268ed4b92092ad53ff
Ranjith Kanakarajan <ranjiaiinnovator@gmail.com>
2025-10-27 22:03:31 +1100

    Removed ZooManager temporarily; updated Main.java to demonstrate Animal hierarchy only

--- Zoo-Management-System/src/main/java/zoo/Main.java
+++ Zoo-Management-System/src/main/java/zoo/Main.java
@@ -2,25 +2,34 @@ package zoo;
 
 public class Main {
     public static void main(String[] args) {
-        ZooManager manager = new ZooManager();
 
+        // Create animal objects
         Mammal lion = new Mammal("Leo", 5);
         Bird parrot = new Bird("Polly", 2);
         Reptile snake = new Reptile("Sly", 3);
 
-        manager.addAnimal(lion);
-        manager.addAnimal(parrot);
-        manager.addAnimal(snake);
-
+        // Display animal info
         System.out.println("\n--- Zoo Status ---");
-        manager.showAllAnimals();
+        System.out.println(lion);
+        System.out.println(parrot);
+        System.out.println(snake);
 
+        // Feeding time
         System.out.println("\n--- Feeding Time ---");
-        manager.feedAllAnimals();
+        lion.eat();
+        parrot.eat();
+        snake.eat();
 
+        // Animal sounds
         System.out.println("\n--- Animal Sounds ---");
         lion.makeSound();
         parrot.makeSound();
         snake.makeSound();
+
+        // Sleeping
+        System.out.println("\n--- Night Time ---");
+        lion.sleep();
+        parrot.sleep();
+        snake.sleep();
     }
 }
